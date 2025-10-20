f601a8b21ae6ae2ac707650841f1efecd78a6316
krexaim <krexaim@gmail.com>
2025-10-20 20:57:42 -0300

    added support for more endpoints

--- dags/scripts/sptrans_api.py
+++ dags/scripts/sptrans_api.py
@@ -4,6 +4,14 @@ from .config import SPTRANS_API_KEY
 SPTRANS_BASE_URL = "https://api.olhovivo.sptrans.com.br/v2.1"
 session = requests.Session()
 
+#------------------
+# ENDPOINTS
+#------------------
+ENDPOINTS = { # {dataset : endpoint}
+    "posicao": "/Posicao"
+    # adicionar endpoints
+}
+
 def authenticate():
     url = f"{SPTRANS_BASE_URL}/Login/Autenticar?token={SPTRANS_API_KEY}"
     response = session.post(url)
@@ -13,7 +21,7 @@ def authenticate():
         raise Exception(f"❌ Authentication failed: {response.status_code} - {response.text}")
 
 def get_bus_positions():
-    url = f"{SPTRANS_BASE_URL}/Posicao"
+    url = f"{SPTRANS_BASE_URL}{posicao}"
     response = session.get(url)
     if response.status_code == 200:
         return response.json()
