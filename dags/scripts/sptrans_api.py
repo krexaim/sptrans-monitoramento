b410a7500782dfece662be0890e31169205ec457
krexaim <krexaim@gmail.com>
2025-10-20 21:44:02 -0300

    changed function to general fetch data

--- dags/scripts/sptrans_api.py
+++ dags/scripts/sptrans_api.py
@@ -20,13 +20,19 @@ def authenticate():
     else:
         raise Exception(f"❌ Authentication failed: {response.status_code} - {response.text}")
 
-def get_bus_positions():
-    url = f"{SPTRANS_BASE_URL}{posicao}"
+def fetch_data(dataset_name):
+    if dataset_name not in ENDPOINTS:
+        raise ValueError(f"Dataset '{dataset_name}' not configured.")
+
+    endpoint = ENDPOINTS[dataset_name]    
+    url = f"{SPTRANS_BASE_URL}{endpoint}"
+    print(f"Fetching {dataset_name} from {url}")
+
     response = session.get(url)
     if response.status_code == 200:
         return response.json()
     else:
-        raise Exception(f"❌ Failed to get positions: {response.status_code} - {response.text}")
+        raise Exception(f"❌ Failed to get {dataset_name}: {response.status_code} - {response.text}")
 
 # adicionar mais funcoes depois
 #def get_bus_previsao():
