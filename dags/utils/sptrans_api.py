8a5cc41450fa42ddf3687f15306c2a763fed8d13
krexaim <krexaim@gmail.com>
2025-11-03 22:28:06 -0300

    fixed endpoint functions

--- dags/utils/sptrans_api.py
+++ dags/utils/sptrans_api.py
@@ -4,15 +4,6 @@ from .config import SPTRANS_API_KEY
 SPTRANS_BASE_URL = "https://api.olhovivo.sptrans.com.br/v2.1"
 session = requests.Session()
 
-#------------------
-# ENDPOINTS
-#------------------
-ENDPOINTS = { # {dataset : endpoint}
-    "posicao": "/Posicao",
-    "linhas": "/Linha/Buscar?termosBusca=a",     
-    "paradas": "/Parada/Buscar?termosBusca=a",
-}
-
 def authenticate():
     url = f"{SPTRANS_BASE_URL}/Login/Autenticar?token={SPTRANS_API_KEY}"
     response = session.post(url)
@@ -21,19 +12,17 @@ def authenticate():
     else:
         raise Exception(f"❌ Authentication failed: {response.status_code} - {response.text}")
 
-def fetch_data(dataset_name):
-    if dataset_name not in ENDPOINTS:
-        raise ValueError(f"Dataset '{dataset_name}' not configured.")
-
-    endpoint = ENDPOINTS[dataset_name]    
+def fetch_data_posicao():
+    endpoint = "/Posicao"  
     url = f"{SPTRANS_BASE_URL}{endpoint}"
-    print(f"Fetching {dataset_name} from {url}")
+    print(f"Fetching posição from {url}")
 
     response = session.get(url)
     if response.status_code == 200:
         return response.json()
     else:
-        raise Exception(f"❌ Failed to get {dataset_name}: {response.status_code} - {response.text}")
+        raise Exception(f"❌ Failed to get posição: {response.status_code} - {response.text}")
+
 
 # adicionar mais funcoes depois
 #def get_bus_previsao():
