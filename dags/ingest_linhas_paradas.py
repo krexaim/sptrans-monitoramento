2f8c971ef2efe2df487651593dea988360290d31
krexaim <krexaim@gmail.com>
2025-11-11 21:38:52 -0300

    updated dag execution order

--- dags/ingest_linhas_paradas.py
+++ dags/ingest_linhas_paradas.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from utils.ingest_bronze import fetch_and_upload
 
@@ -8,8 +9,8 @@ from utils.ingest_bronze import fetch_and_upload
 with DAG(
     dag_id="ingest_linhas_paradas",
     start_date=datetime(2025, 10, 10),
-    schedule_interval=None,
-    catchup=False,
+    schedule_interval="@daily",
+    catchup=True,
     tags=["sptrans", "ingest", "transform", "linhas", "paradas"]
 ) as dag:    
     # Bronze
@@ -40,6 +41,15 @@ with DAG(
         deploy_mode="client",
     )
 
+    # Trigger gold
+
+    trigger_gold_dim = TriggerDagRunOperator(
+        task_id="trigger_transform_gold_dim",
+        trigger_dag_id="transform_gold_dimension",
+        wait_for_completion=False,
+        reset_dag_run=True,
+    )
+
     # Dependências (bronze → silver)
-    [task_linhas, task_paradas] >> task_transform_linhas >> task_transform_paradas
+    [task_linhas, task_paradas] >> task_transform_linhas >> task_transform_paradas >> trigger_gold_dim
      
--- dags/transform_gold.py
+++ dags/transform_gold.py
@@ -1,32 +1,30 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime, timedelta
 
 with DAG(
-    dag_id="transform_gold",
+    dag_id="transform_gold_dimension",
     start_date=datetime(2025, 10, 10),
     schedule_interval=None,
     catchup=False,
     tags=["gtfs", "transform", "gold", "dimension"]
 ) as dag:    
     
-    task_gold= SparkSubmitOperator(
+    # Silver -> Gold 
+    task_gold_linha= SparkSubmitOperator(
         task_id="transform_gold_dim_linha",
         application="/opt/airflow/dags/utils/gold_dim_linha.py",
         conn_id="spark_default",
         name="gold-dim-linha",
-        verbose=True,
         deploy_mode="client",
     )
 
-    # task_gold= SparkSubmitOperator(
-    #     task_id="transform_gtfs_silver_gold",
-    #     application="/opt/airflow/dags/utils/transform_gtfs_silver_gold.py",
-    #     conn_id="spark_default",
-    #     name="gtfs-silver-gold",
-    #     verbose=True,
-    #     deploy_mode="client",
-    # )    
+    task_gold_parada= SparkSubmitOperator(
+        task_id="transform_gold_dim_parada",
+        application="/opt/airflow/dags/utils/gold_dim_parada.py",
+        conn_id="spark_default",
+        name="gold-dim-parada",
+        deploy_mode="client",
+    )
 
-    task_gold
+    task_gold_linha >> task_gold_parada
